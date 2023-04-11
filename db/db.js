e83b40722924e95d101d5683e477d775ca8e7781
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-04-11 18:59:26 -0500

    chore: :card_file_box: change dependency of conexion
    Change connection to mysql2 for database with sequelelize

--- db/db.js
+++ db/db.js
@@ -1,11 +1,20 @@
-const { createPool } = require('mysql2/promise')
+const { Sequelize } = require('sequelize')
 
-const pool = createPool({
+const sequelize = new Sequelize('statemind', 'root', 'root', {
   host: 'localhost',
-  user: 'root',
-  password: 'root',
-  port: 3306,
-  database: 'statemindbd'
+  dialect: 'mysql',
+  port: 3306
 })
 
-module.exports = pool
+async function testConnection () {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+testConnection()
+
+module.exports = sequelize
