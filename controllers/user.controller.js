6487966f6c50abde7e6d9a4fad02e866952f863a
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-04-12 12:42:46 -0500

    feat: :hammer: add get users
    Add get users on controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,24 +1,30 @@
 const { request, response } = require('express')
 const pool = require('../db/db')
 
+const User = require('../model/user.model')
+
 const getUsers = async (req = request, res = response) => {
-  const [result] = await pool.query('SELECT "method GET" AS result')
-  res.status(201).json({
-    msg: result[0]
+  const users = await User.findAll()
+  res.status(200).json({
+    ok: true,
+    status: 201,
+    body: users
   })
 }
 
 const postUsers = async (req = request, res = response) => {
   const { name, email, password } = req.body
-  const [rows] = await pool.query(
-    'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-    [name, email, password]
-  )
-  res.json({
-    id: rows.insertId,
+  await User.sync()
+  const createUser = await User.create({
     name,
     email,
     password
+  })
+  res.status(200).json({
+    ok: true,
+    status: 201,
+    msg: 'Created User',
+    createUser
   })
 }
 
